869ddae74b0c63ff827fb12bb067fa4a6d34ebe4
Mohd Tanzeem <98080739+Mohd-Tanzeem@users.noreply.github.com>
2022-01-26 15:27:37 +0530

    Update hex.js

--- JS Project/1st Project/hex.js
+++ JS Project/1st Project/hex.js
@@ -1,7 +1,11 @@
+//set value to get random color
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// to get btn and color
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+// set event handler with element 
 btn.addEventListener("click", function() {
 let hexColor = "#";
 for(let i = 0;i <6; i++)
@@ -12,6 +16,7 @@ for(let i = 0;i <6; i++)
  document.body.style.backgroundColor = hexColor;
 });
 
+// create function to return highest integer value
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length);
 }
