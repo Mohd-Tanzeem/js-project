192e3892df26e27af801f9423ed8534de77f160f
Mohd Tanzeem <98080739+Mohd-Tanzeem@users.noreply.github.com>
2022-01-26 15:31:02 +0530

    Update app.js

--- JS Project/4th project/app.js
+++ JS Project/4th project/app.js
@@ -1,7 +1,9 @@
+//select items
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
+// add event handler with element toggleBtn
 toggleBtn.addEventListener("click", function () {
   // if (sidebar.classList.contains("show-sidebar")) {
   //   sidebar.classList.remove("show-sidebar");
